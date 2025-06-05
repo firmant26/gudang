31c3cad3dba208bb013d68c6d1426f4cf1766fe4
firmant26 <andayanabwi@gmail.com>
2025-06-05 14:20:49 +0700

    update double linked list
    mengubah lihatBarang dan menambahkan method hapusBarang

--- lib/doubleLinkedList.dart
+++ lib/doubleLinkedList.dart
@@ -73,17 +73,59 @@ class DoubleLinkedListBarang {
     return null;
   }
 
-  void lihatBarang(int kodeBarang) {
-    NodeBarang? node = cariBarang(kodeBarang);
-    if (node != null) {
-      print("------------ DETAIL BARANG: ------------");
-      print("Nama: ${node.data.namaBarang}");
-      print("Kode: ${node.data.kodeBarang}");
-      print("Exp: ${node.data.tanggalBeli}");
-      // Tambahkan field lain sesuai kebutuhan
-    } else {
-      print("Barang dengan kode '$kodeBarang' tidak ditemukan.");
+  void lihatBarang() {
+    if (head == null) {
+      print("Tidak ada barang di gudang.");
+      return;
     }
+    print('------------ DAFTAR BARANG: ------------');
+    NodeBarang? current = head;
+    while (current != null) {
+      print("Nama         : ${current.data.namaBarang}");
+      print("Kode         : ${current.data.kodeBarang}");
+      print("Jumlah       : ${current.data.jumlahBarangDidalam} ${current.data.satuan}");
+      print("Tanggal Beli : ${current.data.tanggalBeli}");
+      print("Harga Beli   : ${current.data.hargaBeli}");
+      print("Harga Jual   : ${current.data.hargaJual}");
+      print("----------------------------------------");
+      current = current.next;
+    }
+
+    // NodeBarang? node = cariBarang(kodeBarang);
+    // if (node != null) {
+    //   print("------------ DETAIL BARANG: ------------");
+    //   print("Nama: ${node.data.namaBarang}");
+    //   print("Kode: ${node.data.kodeBarang}");
+    //   print("Exp: ${node.data.tanggalBeli}");
+    //   // Tambahkan field lain sesuai kebutuhan
+    // } else {
+    //   print("Barang dengan kode '$kodeBarang' tidak ditemukan.");
+    // }
+  }
+
+  void hapusBarang(String kodeBarang) {
+    NodeBarang? current = head;
+    while (current != null) {
+      if (current.data.kodeBarang == kodeBarang) {
+        if (current.prev != null) {
+          current.prev!.next = current.next;
+          print("Barang dengan kode '$kodeBarang' telah dihapus.");
+        } else {
+          head = current.next; // Jika yang dihapus adalah head
+          print("Barang dengan kode '$kodeBarang' telah dihapus.");
+        }
+        if (current.next != null) {
+          current.next!.prev = current.prev;
+          print("Barang dengan kode '$kodeBarang' telah dihapus.");
+        } else {
+          tail = current.prev; // Jika yang dihapus adalah tail
+          print("Barang dengan kode '$kodeBarang' telah dihapus.");
+        }
+        return;
+      }
+      current = current.next;
+    }
+    print("Barang dengan kode '$kodeBarang' tidak ditemukan.");
   }
 
   bool editBarang(int kodeBarang, {String? namaBaru, String? tanggalBeliBaru}) {
@@ -96,4 +138,3 @@ class DoubleLinkedListBarang {
     return false;
   }
 }
-
